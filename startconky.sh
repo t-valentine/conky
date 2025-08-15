64cb7a92adfa1da296d2f3e03bab290a140166f9
t-valentine <tobiasvalentine95@gmail.com>
2025-08-15 09:10:12 -0500

    add comments

--- startconky.sh
+++ startconky.sh
@@ -1,2 +1,4 @@
-conky -c /home/student/Documents/conky.conf 
+# make sure to point towards current folder 
+# (otherwise conky will use default config file)
+conky -c /home/student/Documents/conky/conky.conf 
 
